2a8ace523421fc7b32226bbd9cba3568fab6b19e
paolo flore <paoloflore937@gmail.com>
2024-05-20 14:17:57 +0200

    classe PizzeCategorie per passarla alla view

---
+++ la-mia-pizzeria-static/data/PizzeCategorie.cs
@@ -0,0 +1,8 @@
+﻿namespace la_mia_pizzeria_static.data
+{
+    public class PizzeCategorie
+    {
+        public Pizze Pizze { get; set; }
+        public List<Categoria>? Categorias { get; set; }
+    }
+}
