52d361af1c1231e5c53e085b24f5980436e470ae
paolo flore <paoloflore937@gmail.com>
2024-05-20 14:58:00 +0200

    la view create fatta

--- la-mia-pizzeria-static/data/Categoria.cs
+++ la-mia-pizzeria-static/data/Categoria.cs
@@ -2,7 +2,7 @@
 {
     public class Categoria
     {
-        public int id {  get; set; }
+        public int Id {  get; set; }
         public string CateggoriePizze { get; set; }
         public List<Pizze> PizzeList { get; set;}
     }
